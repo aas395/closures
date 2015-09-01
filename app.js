264b042b9bc46a4f995d70c113bb0445f5839ae2
Aaron Smyth <aas395@gmail.com>
2015-09-01 18:42:46 -0400

    demonstrating another closure example

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 (function($) {
-	var test = "test";
-
-	function myTest(arg1) {
-		//references the global variable "test"
-		test = arg1;
-		var test2 = 'test3';
-	}
-
-	myTest('test2');
-
-	//what does this print out?
-	console.log(window.test);	
+	
 })(jQuery);
 
-var func = function($) {};
-
-func(jQuery)
+function outer() {
+	function inner() {
+		
+	}
+}
