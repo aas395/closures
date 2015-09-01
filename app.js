d04b3c9ee1d7d2612d3109b57488465870560f5f
Aaron Smyth <aas395@gmail.com>
2015-09-01 18:38:10 -0400

    showing first closure

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var test = "test";
+(function($) {
+	var test = "test";
 
-function myTest(arg1) {
-	//references the global variable "test"
-	test = arg1;
-	var test2 = 'test3';
-}
+	function myTest(arg1) {
+		//references the global variable "test"
+		test = arg1;
+		var test2 = 'test3';
+	}
 
-myTest('test2');
+	myTest('test2');
 
-//what does this print out?
-console.log(window.test);
+	//what does this print out?
+	console.log(window.test);	
+})(jQuery);
+
+var func = function($) {};
+
+func(jQuery)
