a2f141745f404de6001e85d4a0249c5c27bd0377
Aaron Smyth <aas395@gmail.com>
2015-09-01 18:32:51 -0400

    changing argument name to illustrate global scope

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var test = "test";
 
-function myTest(test) {
-	//creates a variable in this scope called "test"
-	test = "test2";
+function myTest(arg1) {
+	//references the global variable "test"
+	test = arg1;
+	var test2 = 'test3';
 }
 
-myTest(test);
+myTest('test2');
 
 //what does this print out?
-console.log(test);
+console.log(window.test);
