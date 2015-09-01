6903d9bef94847a151bf3b7608c77ef05eaf2532
Aaron Smyth <aas395@gmail.com>
2015-09-01 18:28:55 -0400

    removing assignment from function

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var test = "test";
 
 function myTest(test) {
+	//creates a variable in this scope called "test"
 	test = "test2";
 }
 
-test = myTest(test);
+myTest(test);
 
 //what does this print out?
 console.log(test);
