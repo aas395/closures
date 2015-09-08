9d1d9680de6effdf8cdec3963e94e0771f95b89c
Aaron Smyth <aas395@gmail.com>
2015-09-08 18:26:09 -0400

    updating to use angular

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-(function($) {
-	
-})(jQuery);
+(function() {
+	var app = angular.module('testApp', []);
 
-function outer() {
-	function inner() {
-		
-	}
-}
+	app.controller('TestCtrl', ['$scope', function($scope) {
+
+		$scope.people = [
+			{firstName: 'Lisa', lastName: 'Wells'},
+			{firstName: 'Sheri', lastName: 'Grossman'},
+			{firstName: 'Thomas', lastName: 'Lutazi'},
+			{firstName: 'Michael', lastName: 'Trollan'},
+			{firstName: 'Ben', lastName: 'Sayeg'},
+			{firstName: 'Alisha', lastName: 'Kassam'}
+		];
+
+		$scope.person = {
+			name: 'You'
+		};
+	}]);
+})();
